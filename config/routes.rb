9af0dedfe68e1b1ffdcd4bb5fe6ba1552615c86d
Cecile Dezy <cecile.dezy@gmail.com>
2018-11-20 19:05:10 +0100

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'tasks', to: 'tasks#index'
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#delete', as: :destroy_task
+
 end
+
+
+#   Prefix Verb   URI Pattern                     Controller#Action
+#     restaurants GET    /restaurants(.:format)          restaurants#index
+#                 POST   /restaurants(.:format)          restaurants#create
+#  new_restaurant GET    /restaurants/new(.:format)      restaurants#new
+# edit_restaurant GET    /restaurants/:id/edit(.:format) restaurants#edit
+#      restaurant GET    /restaurants/:id(.:format)      restaurants#show
+#                 PATCH  /restaurants/:id(.:format)      restaurants#update
+#                 DELETE /restaurants/:id(.:format)      restaurants#destroy
